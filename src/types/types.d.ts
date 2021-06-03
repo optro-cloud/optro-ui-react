daa68cc6bdef36176832fa2f0005ea491b665042
jeffortlessly <jeff.tomband@automation-consultants.com>
2021-06-03 13:19:07 +0100

    :art: Types expanded

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,4 +1,31 @@
+import * as React from "react";
+
 export type LocaleKey = "en" | "fr" | "de" | "es";
+
+export type LicenseType = "board" | "user";
+
+export interface LicenseContext {
+    loading?: boolean;
+    licensed: boolean;
+    expired: boolean;
+    powerupId: string;
+    licenseType: LicenseType;
+    licenseId: string;
+}
+
+export interface LicenseProviderProps {
+    apiKey: string;
+    powerupId: string;
+    children: React.ReactNode;
+}
+
+export interface LicenseConditionalProps {
+    loading?: React.ReactNode;
+    licensed?: React.ReactNode;
+    unlicensed?: React.ReactNode;
+    children?: React.ReactNode;
+}
+
 
 export interface SubscriptionStatusProps {
     isPro: boolean;
