12bdd0e546e0d74e3afe302deeafe6d4f31aa6ae
jeffortlessly <jeff.tomband@automation-consultants.com>
2021-06-03 13:29:35 +0100

    :sparkles: Updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-    input: path.join(__dirname, "src", "subscription-status", "SubscriptionStatus.tsx"),
+    input: path.join(__dirname, "src", "license-status", "LicenseStatus.tsx"),
     output: [
         {
             dir: "dist",
